655e03ac9afdf809d822e49845f7b753dce6808e
Brawer Nuñez <n.eedar@hotmail.com>
2021-10-19 18:08:59 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// Componente controlado, debes pasarle el valor que va a mostrar (con value)
+import { useState } from "react";
+// Guardar multiples valores del formulario con un solo useState
+const App = () => {
+  const [ value, setValue ] = useState({ 
+    normal: 'por defecto normal', 
+    texto: 'por defecto texto', 
+    select: '',
+    check: false,
+    estado: 'feliz',
+  })
+  
+  const handleChange = ({ target }) => {
+    // console.log( e.target.type, e.target.checked )
+    setValue(( state ) => ({
+      ...state,
+      [ target.name ]: target.type === 'checkbox' 
+        ? target.checked 
+        : target.value
+    }))
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  console.log( value )
+
+  return(
+    <div>
+      { value.length < 5 ? <span>Longitud mínima de 5</span> : null }
+      <input 
+        type="text" name="normal" 
+        value={ value.normal } onChange={ handleChange }
+      />
+      <textarea name="texto" value={ value.texto } onChange={ handleChange }/>
+      
+      <select name="select" value={ value.select } onChange={ handleChange }>
+        <option value="">-- Seleccione --</option>
+        <option value="chanchofeliz">Chancho feliz</option>
+        <option value="chanchitofeliz">Chanchito feliz</option>
+        <option value="chanchitotriste">Chanchito triste</option>
+        <option value="felipe">felipe</option>
+      </select>
+
+      <input 
+        type="checkbox" name="check"
+        onChange={ handleChange } checked={ value.check } 
+      />
+
+      <div>
+        <label>Chancho</label>
+        <input 
+          onChange={ handleChange } type="radio" 
+          value="feliz" name="estado" 
+          checked={ value.estado === 'feliz'}
+        /> Feliz
+
+        <input 
+          onChange={ handleChange } type="radio" 
+          value="triste" name="estado"
+          checked={ value.estado === 'triste'}
+        /> Triste
+
+        <input 
+          onChange={ handleChange } type="radio" 
+          value="felipe" name="estado"
+          checked={ value.estado === 'felipe'}
+        /> Felipe
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/uncontrolled-components.js
@@ -0,0 +1,24 @@
+// Ejemplo 1 de formulario NO controlados
+const App = () => {
+  const submit = (e) => {
+    e.preventDefault();
+    const data = Array.from(new FormData(e.target))
+    // Object.fromEntries toma a un arreglo y toma al primer valor 
+    // como key del objeto y el resto en sus valores
+    console.log(Object.fromEntries(data))
+  }
+  return (
+    <form onSubmit={ submit }>
+      <div>
+        <span>
+          lala
+        </span>
+        <input type="text" name='campo' />
+      </div>
+      <input type="text" name='campo2' />
+      <input type="submit" value='Enviar' />
+    </form>
+  )
+}
+
+export default App;
